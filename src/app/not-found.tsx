78753c34b75b38c68d565cb91d63ee976b6a74a9
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-01-08 20:53:16 +0700

    fix: modify meta val

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "not found | yookbeer",
+}
+
 export default function NotFound() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-white">
