1118eaff1f1251d266c7d0e9f5f1d8f92f300edb
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-04-16 15:42:34 +0700

    fix: use correct css color for view image button

--- src/components/actioncell.tsx
+++ src/components/actioncell.tsx
@@ -11,7 +11,7 @@ import { Label } from "./ui/label"
 import { Input } from "./ui/input"
 
 const PersonIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="w-2 h-2 text-black" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" className="w-2 h-2 text-foreground" viewBox="0 0 16 16">
         <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
         <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5z"/>
     </svg>
