ccde83db402be787fad967ae45a66af25ff17ae6
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-12-05 17:15:46 +0700

    feat: include cpe39 and onwards in spotlight

--- src/app/(authorized)/actions.ts
+++ src/app/(authorized)/actions.ts
@@ -3,30 +3,30 @@
 import { auth } from "@/auth"
 import { StudentStatus } from "@/lib/const"
 import { isAuthorized } from "@/lib/rba"
-import { searchThirtyeight } from "@/lib/search"
+import { searchStudents, searchThirtyeight } from "@/lib/search"
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 const S3 = new S3Client({
-  region: "auto",
-  endpoint: `https://${process.env.R2_ACCOUNTID}.r2.cloudflarestorage.com`,
-  credentials: {
-    accessKeyId: process.env.R2_ACCESSKEYID || "",
-    secretAccessKey: process.env.R2_SECRET || "",
-  },
+	region: "auto",
+	endpoint: `https://${process.env.R2_ACCOUNTID}.r2.cloudflarestorage.com`,
+	credentials: {
+		accessKeyId: process.env.R2_ACCESSKEYID || "",
+		secretAccessKey: process.env.R2_SECRET || "",
+	},
 })
 
 export async function getPresignedURLForYookbeerPic(imgName: string) {
-  const session = await auth()
-  if (!session || !isAuthorized(session.user.role!)) return ""
-  const url = await getSignedUrl(S3, new GetObjectCommand({ Bucket: "yookbeer", Key: `${imgName}` }), {
-    expiresIn: 3600,
-  })
-  return url
+	const session = await auth()
+	if (!session || !isAuthorized(session.user.role!)) return ""
+	const url = await getSignedUrl(S3, new GetObjectCommand({ Bucket: "yookbeer", Key: `${imgName}` }), {
+		expiresIn: 3600,
+	})
+	return url
 }
 
 export async function nSearch(q: string) {
-  if (!q.trim()) return []
-  const results = await searchThirtyeight(q)
-  return results.filter((v) => v.status === StudentStatus.ATTENDING)
+	if (!q.trim()) return []
+	const results = await searchStudents(q)
+	return results.filter((v) => v.status === StudentStatus.ATTENDING)
 }
--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,26 +1,49 @@
 import { db } from "@/db"
-import { thirtyeight } from "@/db/schema"
+import { students, thirtyeight } from "@/db/schema"
 import { sql } from "drizzle-orm"
 
 export async function searchThirtyeight(searchTerm: string) {
-  const columns = [
-    thirtyeight.stdid,
-    thirtyeight.nameth,
-    thirtyeight.nameen,
-    thirtyeight.nickth,
-    thirtyeight.nicken,
-    thirtyeight.emailuni,
-    thirtyeight.emailper,
-    thirtyeight.instagram,
-    thirtyeight.discord,
-  ]
+	const columns = [
+		thirtyeight.stdid,
+		thirtyeight.nameth,
+		thirtyeight.nameen,
+		thirtyeight.nickth,
+		thirtyeight.nicken,
+		thirtyeight.emailuni,
+		thirtyeight.emailper,
+		thirtyeight.instagram,
+		thirtyeight.discord,
+	]
 
-  const searchCondition = sql`${sql.join(
-    columns.map((column) => sql`${column} ILIKE ${`%${searchTerm}%`}`),
-    sql` OR `
-  )}`
+	const searchCondition = sql`${sql.join(
+		columns.map((column) => sql`${column} ILIKE ${`%${searchTerm}%`}`),
+		sql` OR `
+	)}`
 
-  const results = await db.select().from(thirtyeight).where(searchCondition).execute()
+	const results = await db.select().from(thirtyeight).where(searchCondition).execute()
 
-  return results
+	return results
+}
+
+export async function searchStudents(searchTerm: string) {
+	const columns = [
+		students.stdid,
+		students.nameth,
+		students.nameen,
+		students.nickth,
+		students.nicken,
+		students.instagram,
+		students.discord,
+	]
+
+	const searchCondition = sql`${sql.join(
+		columns.map((column) => sql`${column} ILIKE ${`%${searchTerm}%`}`),
+		sql` OR `
+	)}`
+
+	const results = await db.select().from(students).where(searchCondition).execute()
+
+	console.log(results)
+
+	return results
 }
